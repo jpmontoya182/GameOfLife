b27e54c13f11ab11f96c08c78f1a00b342aed7a5
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2024-01-30 10:50:15 -0500

    feature - it was finished the first version of calculation of new board

--- Application/Game/Utils/CalculatedNewBoard.cs
+++ Application/Game/Utils/CalculatedNewBoard.cs
@@ -1,26 +1,38 @@
 using Application.Common.Interfaces;
 using Domain.Entities;
+using System.Collections.Generic;
 
 namespace Application.Game.Utils;
 
+class Positions
+{
+    public int x { get; set; }
+    public int y { get; set; }
+}
+
 public class CalculatedNewBoard : ICalculatedNewBoard
 {
-    public CalculatedNewBoard()
-    {
-        
-    }
+    IList<int[]> board = new List<int[]>();
+    IList<int[]> temporalResult = new List<int[]>();
+    int totalLenghtX = 0;
+    int totalLenghtY = 0;
+
 
     public GameItem ProcessBoard(GameItem game)
     {
-        IList<int[]> board = game.Board.ToList();
-        IList<int[]> temporalResult = new List<int[]>();
+        board = game.Board.ToList();
+        temporalResult = game.Board.ToList();
+        totalLenghtX = board.Count();
+        totalLenghtY = board[0].Length;
+
+        
 
         for (var x = 0; x < board.Count(); x++)
         {
             for (int y = 0; y < board[x].Length; y++)
             {
-                Console.WriteLine(board[x][y]);
-                // con la posicion hacer la validacion
+                var positions = CalculatedValidPositions(x, y);
+                CalculatedValues(positions, x, y);
             }
         }
 
@@ -36,14 +48,40 @@ public class CalculatedNewBoard : ICalculatedNewBoard
     /// con las coordenadas, validamos nuestras reglas de negocio y contamos los vivos
     /// necesitamos compartir la matriz entre los procedimientos ?
     /// </summary>
-    private void ValidatePositions()
-    { 
-    
+    private List<Positions> CalculatedValidPositions(int posX, int posY)
+    {
+        var validPositions =    (from x in Enumerable.Range(posX - 1, 3)
+                                from y in Enumerable.Range(posY - 1, 3)
+                                where x >= 0 && y >= 0 && x < totalLenghtX && y < totalLenghtY
+                                select new Positions { x = x, y = y }).ToList();
+
+
+        var originalPosition = validPositions.Where(p => p.y == posY && p.x == posX).ToList();
+        var result = validPositions.Except(originalPosition).ToList();
+
+        return result;
+    }
+
+
+    private void CalculatedValues(List<Positions> positions, int posX, int posY)
+    {
+        int liveNeighbors = 0; 
+        foreach (var position in positions)
+        {  
+            if (board[position.x][position.y] == 1)
+            {
+                liveNeighbors++;
+            }
+            
+        }    
+        temporalResult[posX][posY] = liveNeighbors;
     }
 
 
     private IList<int[]> CreateNewBoard(IList<int[]> temporalResult)
     {
+        if (temporalResult.Count() == 0) return null;
+   
         for (var x = 0; x < temporalResult.Count(); x++)
         {
             for (int y = 0; y < temporalResult[x].Length; y++)
@@ -61,6 +99,5 @@ public class CalculatedNewBoard : ICalculatedNewBoard
         }
 
         return temporalResult;
-
     }
 }
